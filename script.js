af18cd1ffb6147fe100d410f03ebed39bcc8f9ad
maurolps <boroengenharia@gmail.com>
2023-08-22 17:55:56 -0300

    Change function names, added remove button

--- script.js
+++ script.js
@@ -32,22 +32,35 @@ Book.prototype.info = function () {
   return this.title+" by "+this.author+", "+this.pages+" pages, "+read;
 }
 
-function addBookToLibrary(book) {
-  // myLibrary.push(book);
-  console.log(book);
-  let bookCard = document.createDocumentFragment('div');
-  bookCard.className = "book-card";
-  Object.values(book).forEach((element) => {
-    let p = document.createElement('p');
-    p.textContent = element;
-    bookCard.appendChild(p);
-    console.log(p);
-  });
+function removeBook() {
+ this.parentElement.remove();
+}
 
+function updateDisplay(book) {
+  // myLibrary.push(book);
+  // console.log(book);
+  let bookCardFragment = document.createDocumentFragment();
+  let bookCard = document.createElement('div');
+  let btnRemove = document.createElement('button');
+  bookCard.className = "book-card";
+  Object.keys(book).forEach((key) => {
+    let p = document.createElement('p');
+    p.textContent = key+": "+book[key];
+    bookCardFragment.appendChild(p);
+  });
+  btnRemove.className = "btn-remove";
+  btnRemove.innerHTML = "Remove";
+  btnRemove.addEventListener('click', removeBook);
+  bookCard.appendChild(bookCardFragment);
+  bookCard.appendChild(btnRemove);
   document.body.appendChild(bookCard);
 }
 
-addBookToLibrary(myLibrary[1]);
+function addBookToLibrary(book) {
+  
+}
+
+updateDisplay(myLibrary[1]);
 
 // const book1 = new Book('The Hobbit','J.R.R Tolkien','295',true);
 // addBookToLibrary(book1);
